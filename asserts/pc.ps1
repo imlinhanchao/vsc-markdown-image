1f22cfe781652a5438912cd7a10bf631c7bc318e
Tenormis <61572102+Tenormis@users.noreply.github.com>
2025-02-09 00:34:36 +0800

    fix pc:ps1: compatible with snipaste

--- asserts/pc.ps1
+++ asserts/pc.ps1
@@ -5,34 +5,66 @@ chcp 65001
 
 Add-Type -Assembly PresentationCore
 
-if (-not $imagePath) {
-    "image path unspecified"
-    Exit 1
-}
-
-$img = [Windows.Clipboard]::GetImage()
-
-if ($null -eq $img) {
-    $files = [Windows.Clipboard]::GetFileDropList()
-    if ($null -eq $files) {
-        "no image"
+$image = [System.Windows.Clipboard]::GetImage()
+if (-not $image) {
+    $fileDropList = [System.Windows.Clipboard]::GetFileDropList()
+    if (-not $fileDropList) {
+        Write-Host "no image in clipboard"
         Exit 1
     }
-    $files
+    Write-Host $fileDropList
     Exit 0
 }
 
-try {
-    $stream = [IO.File]::Open($imagePath, [IO.FileMode]::Create)
-    $fcb = New-Object Windows.Media.Imaging.FormatConvertedBitmap($img, $img.Format, $null, 100)
-    $encoder = New-Object Windows.Media.Imaging.PngBitmapEncoder
-    $encoder.Frames.Add([Windows.Media.Imaging.BitmapFrame]::Create($fcb)) | Out-Null
-    $encoder.Save($stream)
-}
-finally {
-    if ($stream) { $stream.Dispose() }
+if (-not $imagePath) {
+    Write-Host "image path unspecified"
+    Exit 1
 }
 
-$imagePath
+function Is-Bgra32AndMissingAlpha {
+    param([System.Windows.Media.Imaging.BitmapSource] $source)
 
-Exit 0
+    if ($source.Format -ne [System.Windows.Media.PixelFormats]::Bgra32) {
+        return $false
+    }
+
+    $stride = $source.PixelWidth * 4
+    $pixelData = New-Object byte[] ($stride * $source.PixelHeight)
+
+    try {
+        $source.CopyPixels($pixelData, $stride, 0)
+    } catch {
+        return $false
+    }
+
+    for ($i = 0; $i -lt $pixelData.Length; $i += 4) {
+        if ($pixelData[$i + 3] -gt 0) {
+            return $false
+        }
+    }
+
+    return $true
+}
+
+$targetFormat = if (-not @(
+    [System.Windows.Media.PixelFormats]::Bgra32,
+    [System.Windows.Media.PixelFormats]::Pbgra32,
+    [System.Windows.Media.PixelFormats]::Rgba64,
+    [System.Windows.Media.PixelFormats]::Prgba64,
+    [System.Windows.Media.PixelFormats]::Rgba128Float
+) -contains $image.Format) {
+    [System.Windows.Media.PixelFormats]::Rgb24
+} elseif (Is-Bgra32AndMissingAlpha -source $image) {
+    [System.Windows.Media.PixelFormats]::Rgb24
+} else {
+    [System.Windows.Media.PixelFormats]::Bgra32
+}
+
+$fcb = New-Object Windows.Media.Imaging.FormatConvertedBitmap($image, $targetFormat, $null, 0)
+$stream = [System.IO.File]::Open($imagePath, "Create")
+$encoder = New-Object Windows.Media.Imaging.PngBitmapEncoder
+$encoder.Frames.Add([System.Windows.Media.Imaging.BitmapFrame]::Create($fcb)) | Out-Null
+$encoder.Save($stream) | Out-Null
+$stream.Dispose() | Out-Null
+
+Write-Host $imagePath
